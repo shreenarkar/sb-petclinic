94489bef9df752911bb62e419ea2cb1701f81c40
Shreyash <shreyashnarkar@gmail.com>
2025-06-29 22:54:24 +0530

    Minor changes in source file

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/*New app*/
 package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
